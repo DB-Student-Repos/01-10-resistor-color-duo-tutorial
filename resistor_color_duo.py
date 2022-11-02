8f8bd72ef5ecb2bf3c51bcef01d2610444939705
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-11-02 16:16:13 +0100

    Update resistor_color_duo.py

--- resistor_color_duo.py
+++ resistor_color_duo.py
@@ -1,2 +1,31 @@
 def value(colors):
-    pass
+    x= color_code(colors[0])
+    y= color_code(colors[1])
+    z=(x*10)+y
+    return z
+                  
+                  
+def color_code(color):
+    y=(color.lower())
+    if(y=="black"):
+        return 0
+    elif(y=="brown"):
+        return 1
+    elif(y=="red"):
+        return 2
+    elif(y=="orange"):
+        return 3
+    elif(y=="yellow"):
+        return 4
+    elif(y=="green"):
+        return 5
+    elif(y=="blue"):
+        return 6
+    elif(y=="violet"):
+        return 7
+    elif(y=="grey"):
+        return 8
+    elif(y=="white"):
+        return 9
+    else:
+        return "RaiseValueError"
